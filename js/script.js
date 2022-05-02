5045cbc7571b682fcb692c9a91d3997477c91f60
RenatoConfuorto <renato.confuorto@outlook.it>
2022-05-02 19:56:53 +0200

    Milestone 1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const iconContainer = document.querySelector('main > .container');
+
+
+//inserire in pagina un elemento per ogni oggetto dell'array
+icons.forEach( element => {
+  const cell = document.createElement('div');
+  cell.classList.add('icon-cell');
+
+  const icon = document.createElement('i');
+  icon.classList.add(`${element.family}`, `${element.prefix}${element.name}`);
+  cell.append(icon);
+
+  const name = document.createElement('h3');
+  name.innerText = `${element.name}`;
+  cell.append(name);
+
+  iconContainer.appendChild(cell);
+});
